00dd3c2858e1b2db9bda5595a5cebfa20a44e82e
stefanmenzelatuniulm <stefan.menzel@uni-ulm.de>
2024-03-18 22:35:09 +0100

    .

--- phaseCorrectAndFit.m
+++ phaseCorrectAndFit.m
@@ -9,11 +9,12 @@ gamma13C = 10.7084*10^6; %gyromagnetic ration of 13C (Hz/T)
 B0 = 3; %B0 of the scanner (T)
 rf_center_ppm = 169.7; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
 bw = 1200; %Bandwidth of the RF pulse (Hz)
-chemicalSpecies = "Bicarbonate Alanine Lactate"; %Name(s) of the chemical species in the spectrum
-scaleHzFit = 10; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
+chemicalSpecies = "Lactate"; %Name(s) of the chemical species in the spectrum
+scaleHzFit = 15; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
+scalePpmFit = 0.5;
 increaseSliderStepResolutionFactor = 32; %Increases the default slider resolution by this factor, relevant for phase correction
 annotationXOffset = 0; %Offset of fit parameter annotation in X direction, if there is significant overlap with the plot
-path = "C:\Users\Stefan Menzel\Desktop\Matlab\MR_Data\13_03_2023\SpectrumBicAlaLac\27"; %Path to data
+path = "C:\Users\Stefan Menzel\Desktop\Matlab\MR_Data\13_03_2023\SpectrumLac\31"; %Path to data
 attemptLorentzianFit = true; %Attempt to fit single Lorentzian
 attemptFIDFit = true;
 
@@ -71,9 +72,9 @@ phaseAngleRad = angle(spectrum(maxIndex));
 Data = Data*exp(-1i*phaseAngleRad);
 spectrum = fftshift(fft(Data));
 
-%Plot phase angle of spectrum %Macht random Phasensprünge um pi
+%Plot phase angle of spectrum 
 fig = figure('WindowState', 'maximized');
-plot(X_Sample, angle(spectrum)); %Rough baseline correction
+plot(X_Sample, angle(spectrum)+pi); 
 legend("Phase angle", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
 title("Phase angle of the spectrum of "+chemicalSpecies);
 xlabel("Sample", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
@@ -99,7 +100,6 @@ close all;
 disp("Adjust 0th and 1st order phase correction");
 S.fh = figure('WindowState', 'maximized');
 S.ax = axes('unit', 'normalized', 'position', [0.05 0.15 0.9 0.8]);
-S.angleFunction = @(x, phi0, phi1, pivot, N) phi0+phi1*(x-pivot)/N;
 S.X_Sample = X_Sample;
 S.Data = Data;
 S.phi0 = 0;
@@ -134,10 +134,9 @@ phi1 = get(S.phi1Slider, "Value");
 pivot = get(S.pivotSlider, "Value");
 
 %Apply 0th and 1st order phase correction
-Phi = transpose(phi0 + phi1*(S.X_Sample-pivot));
-realData = real(Data).*cos(Phi)-imag(Data).*sin(Phi); %https://pubmed.ncbi.nlm.nih.gov/28218950/
-imagData = real(Data).*sin(Phi)+imag(Data).*cos(Phi);
-Data = realData + 1i*imagData;
+[mn, index] = min(abs(X_Sample-pivot));
+Phi = transpose(angleFunction(X_Sample, phi0, phi1, X_Sample(index), length(X_Sample)));
+Data = Data.*exp(-1i*Phi);
 sReal = real(fftshift(fft(Data)));
 
 %Baseline correction and normalization -> Sinnvoll? Tobi fragen
@@ -182,19 +181,19 @@ fig = figure('WindowState', 'maximized');
 ax = gca;
 plot(ax, X_ppm_rel, sReal);
 title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-fitfunction="(1/pi)*((FWHM/2)/((x/"+num2str(scaleHzFit)+"-x0/"+num2str(scaleHzFit)+")^2+(FWHM/2)^2))";
+fitfunction="(1/pi)*((FWHM/2)/((x/"+num2str(scalePpmFit)+"-x0/"+num2str(scalePpmFit)+")^2+(FWHM/2)^2))";
 coeffs=["FWHM" "x0"];
-options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf],'Upper',[inf inf],'StartPoint',[10 round(mean(X_ppm_rel))]);
+options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf],'Upper',[inf inf],'StartPoint',[1 round(mean(X_ppm_rel))]);
 fttype = fittype(fitfunction,coefficients=coeffs);
 X_ppm_t = transpose(X_ppm_rel);
 if attemptLorentzianFit
-    ft=fit(X_Hz_t,double(sReal),fttype,options);
+    ft=fit(X_ppm_t,double(sReal),fttype,options);
     coeffs = coeffnames(ft);
     coeffvals = coeffvalues(ft);
     ci = confint(ft,0.95);
-    str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{1},coeffvals(1)*scaleHzFit,ci(:,1)*scaleHzFit); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
+    str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{1},coeffvals(1)*scalePpmFit,ci(:,1)*scalePpmFit); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
     str2 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{2},coeffvals(2),ci(:,2)); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
-    annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Fit coefficients with 95% confidence bounds: ', strtrim(str2+"   (ppm)"), strtrim(strrep(str2,"x0","\omega_0")+"   (ppm)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",8);
+    annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Fit coefficients with 95% confidence bounds: ', strtrim(str1+"   (ppm)"), strtrim(strrep(str2,"x0","\omega_0")+"   (ppm)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",8);
     hold on;
     plot(ax,ft,"r");
     legend("Amplitude (a. u.)", "Fit with Lorentzian", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
@@ -218,13 +217,18 @@ function SliderCB(aSlider, EventData, Param)
 end
 
 function update(S)
-    angle = transpose(S.angleFunction(S.X_Sample, S.phi0, S.phi1, S.pivot, length(S.X_Sample)));
-    realData = real(S.Data).*cos(angle)-imag(S.Data).*sin(angle); %https://pubmed.ncbi.nlm.nih.gov/28218950/
-    imagData = real(S.Data).*sin(angle)+imag(S.Data).*cos(angle);
-    Data = realData + 1i*imagData;
-    realSpectrum = real(fftshift(fft(Data)));
     [mn, index] = min(abs(S.X_Sample-S.pivot));
-    set(S.p1, 'YData', realSpectrum);  
+    set(S.p3, "Xdata", S.X_Sample(index));
+    realSpectrum = real(fftshift(fft(S.Data)));
+    set(S.p3, "Ydata", realSpectrum(index));
+    Phi = transpose(angleFunction(S.X_Sample, S.phi0, S.phi1, S.X_Sample(index), length(S.X_Sample)));
+    S.Data = S.Data.*exp(-1i*Phi);
+    realSpectrum = real(fftshift(fft(S.Data)));
+    set(S.p1, 'YData', realSpectrum); 
     set(S.p3, "Xdata", S.X_Sample(index));
     set(S.p3, "Ydata", realSpectrum(index));
+end
+
+function phi = angleFunction(x, phi0, phi1, pivot, N)
+    phi = phi0+phi1*(x-pivot)/N;
 end

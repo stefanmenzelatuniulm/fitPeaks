ec38ef7f7ffd774db88721ea606a813ad9297d54
stefanmenzelatuniulm <stefan.menzel@uni-ulm.de>
2024-03-27 14:09:32 +0100

    .

--- phaseCorrectAndFit.m
+++ phaseCorrectAndFit.m
@@ -7,15 +7,15 @@ clc;
 TR = 60000; %ms
 gamma13C = 10.7084*10^6; %gyromagnetic ration of 13C (Hz/T)
 B0 = 3; %B0 of the scanner (T)
-rf_center_ppm = 173.66; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
-bw = 500; %Bandwidth of the RF pulse (Hz)
+rf_center_ppm = 169.7; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
+bw = 1200; %Bandwidth of the RF pulse (Hz)
 chemicalSpecies = "Bicarbonate, Alanine, Lactate"; %Name(s) of the chemical species in the spectrum
 scaleHzFit = 10; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
 scalePpmFit = 0.28;
 increaseSliderStepResolutionFactor = 32; %Increases the default slider resolution by this factor, relevant for phase correction
 increasePhi1LimitsFactor = 5;
 annotationXOffset = 0; %Offset of fit parameter annotation in X direction, if there is significant overlap with the plot
-path = "C:\Users\menze\Desktop\Matlab\MR_Data\2024_03_13\SpectrumBicAlaLac\27"; %Path to data
+path = "C:\Users\Stefan Menzel\Desktop\Matlab\MR_Data\2024_03_13\SpectrumBicAlaLac\27"; %Path to data
 attemptLorentzianFit = false; %Attempt to fit single Lorentzian
 attemptFIDFit = false;
 

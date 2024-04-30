3ef191dd835b4cc41af44e4fb979d4065c605f97
stefanmenzelatuniulm <stefan.menzel@uni-ulm.de>
2024-04-30 17:08:11 +0200

    .

--- phaseCorrectAndFit.m
+++ phaseCorrectAndFit.m
@@ -4,19 +4,20 @@ clc;
 
 %-------------SETTINGS-------------
 
-TR = 30000; %ms
+TR = 60000; %ms
 gamma13C = 10.7084*10^6; %gyromagnetic ration of 13C (Hz/T)
 B0 = 3; %B0 of the scanner (T)
-rf_center_ppm = 158.92; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
-bw = 500; %Bandwidth of the RF pulse (Hz)
-chemicalSpecies = "Bicarbonate"; %Name(s) of the chemical species in the spectrum
+rf_center_ppm = 170.54; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
+bw = 1000; %Bandwidth of the RF pulse (Hz)
+chemicalSpecies = "Bicarbonate, Urea, Alanine, Lactate"; %Name(s) of the chemical species in the spectrum
 scaleHzFit = 10; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
 scalePpmFit = 0.085;
 increaseSliderStepResolutionFactor = 32; %Increases the default slider resolution by this factor, relevant for phase correction
 increasePhi1LimitsFactor = 5;
 annotationXOffset = 0; %Offset of fit parameter annotation in X direction, if there is significant overlap with the plot
-path = "C:\Users\Stefan Menzel\Desktop\Matlab\MR_Data\2024_04_19\Bicarbonate\SpecBic\108"; %Path to data
-attemptLorentzianFit = true; %Attempt to fit single Lorentzian
+floorOffset = 0.015;
+path = "C:\Users\Stefan Menzel\Desktop\Matlab\MR_Data\2024_04_19\Overview\140"; %Path to data
+attemptLorentzianFit = false; %Attempt to fit single Lorentzian
 attemptFIDFit = false;
 
 %-------------END OF SETTINGS-------------
@@ -88,9 +89,9 @@ saveas(fig, path+chemicalSpecies+"_phase.svg");
 
 %Plot modulus of spectrum
 fig = figure('WindowState', 'maximized');
-plot(X_ppm_rel, (abs(spectrum)-median(abs(spectrum)))/max(abs(spectrum)-median(abs(spectrum)))); %Rough baseline correction
+plot(X_ppm_rel, floorOffset+(abs(spectrum)-median(abs(spectrum)))/max(abs(spectrum)-median(abs(spectrum)))); %Rough baseline correction
 legend("Spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
-title("Modulus of the spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
+title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
 xlim([min(X_ppm_rel) max(X_ppm_rel)]);
 xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
 ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);

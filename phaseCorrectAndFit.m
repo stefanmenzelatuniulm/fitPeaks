669da7d30d3b2953dccb279c0ce4e331c372776e
Stefan <menzelste@gmx.de>
2024-07-05 23:10:26 +0200

    .

--- phaseCorrectAndFit.m
+++ phaseCorrectAndFit.m
@@ -4,19 +4,19 @@ clc;
 
 %-------------SETTINGS-------------
 
-TR = 60000; %ms
+TR = 30000; %ms
 gamma13C = 10.7084*10^6; %gyromagnetic ration of 13C (Hz/T)
 B0 = 3; %B0 of the scanner (T)
-rf_center_ppm = 170.54; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
-bw = 1000; %Bandwidth of the RF pulse (Hz)
-chemicalSpecies = "Bicarbonate, Urea, Alanine, Lactate"; %Name(s) of the chemical species in the spectrum
+rf_center_ppm = 180.43; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
+bw = 500; %Bandwidth of the RF pulse (Hz)
+chemicalSpecies = "Lactate"; %Name(s) of the chemical species in the spectrum
 scaleHzFit = 10; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
 scalePpmFit = 0.085;
 increaseSliderStepResolutionFactor = 32; %Increases the default slider resolution by this factor, relevant for phase correction
 increasePhi1LimitsFactor = 5;
 annotationXOffset = 0; %Offset of fit parameter annotation in X direction, if there is significant overlap with the plot
 floorOffset = 0.015;
-path = "C:\Users\menze\Desktop\Matlab\MR_Data\140"; %Path to data
+path = "C:\Users\menze\Desktop\Matlab\MR_Data\105"; %Path to data
 attemptLorentzianFit = false; %Attempt to fit single Lorentzian
 attemptFIDFit = false;
 

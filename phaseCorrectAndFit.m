7a83a06a849b79f7e3641c763ea8b0354de0066d
stefanmenzelatuniulm <stefan.menzel@uni-ulm.de>
2025-03-04 18:49:33 +0100

    .

--- phaseCorrectAndFit.m
+++ phaseCorrectAndFit.m
@@ -4,22 +4,27 @@ clc;
 
 %-------------SETTINGS-------------
 
-TR = 2000; %ms
-%gamma13C = 10.7084*10^6; %gyromagnetic ration of 13C (Hz/T)
-gamma13C = 42.577*10^6;
+TR = 100000; %ms
+gamma13C = 10.7084*10^6; %gyromagnetic ration of 13C (Hz/T)
+%gamma13C = 42.577*10^6;
 B0 = 3; %B0 of the scanner (T)
-rf_center_ppm = 4.7; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
-bw = 2000; %Bandwidth of the RF pulse (Hz)
-chemicalSpecies = "Eisdosen Phantom"; %Name(s) of the chemical species in the spectrum
-scaleHzFit = 10; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
-scalePpmFit = 0.085;
+rf_center_ppm = 170.54; %Center of the RF pulse (ppm) %IST SCHON RELATIV ZU TMS
+bw = 1000; %Bandwidth of the RF pulse (Hz)
+chemicalSpecies = "Bicarbonate, Ethylacetate, Alanine, Lactate"; %Name(s) of the chemical species in the spectrum
+scaleHzFit = 15; %Scaling factor for the x axis only in the context of the Lorentzian fit (CARE: very sensitive)
+scalePpmFit = 0.9;
 increaseSliderStepResolutionFactor = 32; %Increases the default slider resolution by this factor, relevant for phase correction
 increasePhi1LimitsFactor = 5;
 annotationXOffset = 0; %Offset of fit parameter annotation in X direction, if there is significant overlap with the plot
 floorOffset = 0.015;
-path = "C:\Users\menze\Desktop\Matlab\IDEAL\ME bssfp data\2024_11_11\Reconstructed\31"; %Path to data
-attemptLorentzianFit = false; %Attempt to fit single Lorentzian
+path = "D:\Matlab\MR_Data\Preliminary Measurements\Overview\WithEthylacetate\198"; %Path to data
+attemptLorentzianFit = true; %Attempt to fit single Lorentzian
 attemptFIDFit = false;
+notOutlierDomain = [165 170];
+ppmyoffset = -0.03;
+minPeakHeight = 0.489;
+additionalFileString="EthylacetateFit";
+useFindPeaks = false;
 
 %-------------END OF SETTINGS-------------
 
@@ -29,37 +34,37 @@ load(path+"\data.mat");
 %Average over the number of averages 
 Data = double(mean(squeeze(Data),2));
 
-fig = figure('WindowState', 'maximized');
-X_FID = linspace(1,TR,length(Data));
-X_FID_t = transpose(linspace(1,TR,length(Data)));
-plot(X_FID, abs(Data)/max(abs(Data)));
-legend("FID", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
+% fig = figure('WindowState', 'maximized');
+% X_FID = linspace(1,TR,length(Data));
+% X_FID_t = transpose(linspace(1,TR,length(Data)));
+% plot(X_FID, abs(Data)/max(abs(Data)));
+% legend("FID", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
+% 
+% if attemptFIDFit
+%     fitfunction="C+M0*exp(-x/T2s)";
+%     coeffs=["C" "M0" "T2s"];
+%     options=fitoptions('Method','NonlinearLeastSquares','Lower',[0 0 0],'Upper',[1 1 inf],'StartPoint',[0 0 1]);
+%     fttype = fittype(fitfunction,coefficients=coeffs);
+%     ft=fit(X_FID_t,abs(Data)/max(abs(Data)),fttype,options);
+%     coeffvals = coeffvalues(ft);
+%     ci = confint(ft,0.95);
+%     str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',"T_2^*",coeffvals(3),ci(:,3));
+%     annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Relevant fit coefficient with 95% confidence bounds: ', strtrim(str1+"   (ms)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",18);
+%     hold on;
+%     ax = gca;
+%     plot(ax,ft,"r");
+%     legend("FID", "Fit with $$C+M_0 e^{-\frac{t}{T_2^*}}$$", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
+% end
 
-if attemptFIDFit
-    fitfunction="C+M0*exp(-x/T2s)";
-    coeffs=["C" "M0" "T2s"];
-    options=fitoptions('Method','NonlinearLeastSquares','Lower',[0 0 0],'Upper',[1 1 inf],'StartPoint',[0 0 1]);
-    fttype = fittype(fitfunction,coefficients=coeffs);
-    ft=fit(X_FID_t,abs(Data)/max(abs(Data)),fttype,options);
-    coeffvals = coeffvalues(ft);
-    ci = confint(ft,0.95);
-    str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',"T_2^*",coeffvals(3),ci(:,3));
-    annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Relevant fit coefficient with 95% confidence bounds: ', strtrim(str1+"   (ms)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",8);
-    hold on;
-    ax = gca;
-    plot(ax,ft,"r");
-    legend("FID", "Fit with $$C+M_0 e^{-\frac{t}{T_2^*}}$$", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
-end
-
-title("FID of "+chemicalSpecies);
-xlabel("$t$ (ms)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-xlim([min(X_FID) max(X_FID)]);
-ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-saveas(fig, path+chemicalSpecies+"_FID.fig");
-saveas(fig, path+chemicalSpecies+"_FID.svg");
-saveas(fig, path+chemicalSpecies+"_FID.png");
-
-close all;
+% title("FID of "+chemicalSpecies);
+% xlabel("$t$ (ms)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+% xlim([min(X_FID) max(X_FID)]);
+% ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+% saveas(fig, path+chemicalSpecies+"_FID.fig");
+% saveas(fig, path+chemicalSpecies+"_FID.svg");
+% saveas(fig, path+chemicalSpecies+"_FID.png");
+% 
+% close all;
 
 %Determine spectrum
 spectrum = fftshift(fft(Data));
@@ -78,27 +83,30 @@ phaseAngleRad = angle(spectrum(maxIndex));
 Data = Data*exp(-1i*phaseAngleRad);
 spectrum = fftshift(fft(Data));
 
-%Plot phase angle of spectrum 
-fig = figure('WindowState', 'maximized');
-plot(X_Sample, angle(spectrum)+pi); 
-legend("Phase angle", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
-title("Phase angle of the spectrum of "+chemicalSpecies);
-xlim([min(X_Sample) max(X_Sample)]);
-xlabel("Sample", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-ylabel("Phase angle (rad)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-
-saveas(fig, path+chemicalSpecies+"_phase.fig");
-saveas(fig, path+chemicalSpecies+"_phase.svg");
-saveas(fig, path+chemicalSpecies+"_phase.png");
+% %Plot phase angle of spectrum 
+% fig = figure('WindowState', 'maximized');
+% plot(X_Sample, angle(spectrum)+pi); 
+% legend("Phase angle", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
+% title("Phase angle of the spectrum of "+chemicalSpecies);
+% xlim([min(X_Sample) max(X_Sample)]);
+% xlabel("Sample", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+% ylabel("Phase angle (rad)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+% 
+% saveas(fig, path+chemicalSpecies+"_phase.fig");
+% saveas(fig, path+chemicalSpecies+"_phase.svg");
+% saveas(fig, path+chemicalSpecies+"_phase.png");
 
 %Plot modulus of spectrum
 fig = figure('WindowState', 'maximized');
 plot(X_ppm_rel, floorOffset+(abs(spectrum)-median(abs(spectrum)))/max(abs(spectrum)-median(abs(spectrum)))); %Rough baseline correction
-legend("Spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
-title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 16);
+legend("Spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
+title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
 xlim([min(X_ppm_rel) max(X_ppm_rel)]);
-xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
+xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+ax = gca;
+ax.FontSize = 18;
+drawnow;
 
 saveas(fig, path+chemicalSpecies+"_ppm_Modulus.fig");
 saveas(fig, path+chemicalSpecies+"_ppm_Modulus.svg");
@@ -133,10 +141,13 @@ txtPivot = uicontrol('Style','text', 'unit', 'normalized', 'position', [0 0.023 
 update(S);
 guidata(S.fh, S);
 title("Click on sliders, then select pivot and adjust sliders with arrow keys for 0th and 1st order phase correction, then minimize figure and press Enter in Command Window");
-legend("Real part of the spectrum", "Median of the real part of the spectrum $$\approx$$ noise floor", "Pivot", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest")
+legend("Real part of the spectrum", "Median of the real part of the spectrum $$\approx$$ noise floor", "Pivot", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 12, "Location", "Northwest")
 xlim([min(X_ppm_rel) max(X_ppm_rel)]);
-xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14); 
-ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
+xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 12); 
+ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 12);
+ax = gca;
+ax.FontSize = 12;
+drawnow;
 
 pause;
 
@@ -155,53 +166,57 @@ sReal = real(fftshift(C.*fft(tempData)));
 sReal = sReal-median(sReal); %Wenn das noise level konsequent über 0 wäre, wäre doch der median entsprechend über 0? nicht mean verwenden, da peaks nicht mit einbezogen werden sollen
 sReal = sReal/max(sReal);
 
-%Plot and fit Lorentzian
-fig = figure('WindowState', 'maximized');
-ax = gca;
-plot(ax, X_Hz_rel, sReal);
-title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 16);
-fitfunction="(1/pi)*((FWHM/2)/((x/"+num2str(scaleHzFit)+"-x0/"+num2str(scaleHzFit)+")^2+(FWHM/2)^2))";
-coeffs=["FWHM" "x0"];
-options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf],'Upper',[inf inf],'StartPoint',[10 mean(X_Hz_rel)]);
-fttype = fittype(fitfunction,coefficients=coeffs);
-X_Hz_t = transpose(X_Hz_rel);
-if attemptLorentzianFit
-    ft=fit(X_Hz_t,sReal,fttype,options);
-    coeffs = coeffnames(ft);
-    coeffvals = coeffvalues(ft);
-    ci = confint(ft,0.95);
-    str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{1},coeffvals(1)*scaleHzFit,ci(:,1)*scaleHzFit); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
-    str2 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{2},coeffvals(2),ci(:,2)); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
-    str3 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',"Therefore T_2^*",1000/(coeffvals(1)*scaleHzFit*pi),1000*ci(:,1)*scaleHzFit/(pi*(coeffvals(1)*scaleHzFit)^2));
-    annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Fit coefficients with 95% confidence bounds: ', strtrim(str1+"   (Hz)"), strtrim(strrep(str2,"x0","\omega_0")+"   (Hz)"), strtrim(str3+"   (ms)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",8);
-    hold on;
-    plot(ax,ft,"r");
-    legend("Real part of the spectrum", "Fit with Lorentzian", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
-else
-    legend("Real part of the spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
-end
-xlim([min(X_Hz_rel) max(X_Hz_rel)]);
-xlabel("Chemical Shift (Hz)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14); 
-ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
-
-saveas(fig, path+chemicalSpecies+"_Hz.fig");
-saveas(fig, path+chemicalSpecies+"_Hz.svg");
-saveas(fig, path+chemicalSpecies+"_Hz.png");
-
-close all;
+% %Plot and fit Lorentzian
+% fig = figure('WindowState', 'maximized');
+% ax = gca;
+% plot(ax, X_Hz_rel, sReal);
+% title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+% fitfunction="(1/pi)*((FWHM/2)/((x/"+num2str(scaleHzFit)+"-x0/"+num2str(scaleHzFit)+")^2+(FWHM/2)^2))";
+% coeffs=["FWHM" "x0"];
+% options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf],'Upper',[inf inf],'StartPoint',[10 mean(X_Hz_rel)]);
+% fttype = fittype(fitfunction,coefficients=coeffs);
+% X_Hz_t = transpose(X_Hz_rel);
+% if attemptLorentzianFit
+%     ft=fit(X_Hz_t,sReal,fttype,options);
+%     coeffs = coeffnames(ft);
+%     coeffvals = coeffvalues(ft);
+%     ci = confint(ft,0.95);
+%     str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{1},coeffvals(1)*scaleHzFit,ci(:,1)*scaleHzFit); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
+%     str2 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{2},coeffvals(2),ci(:,2)); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
+%     str3 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',"Therefore T_2^*",1000/(coeffvals(1)*scaleHzFit*pi),1000*ci(:,1)*scaleHzFit/(pi*(coeffvals(1)*scaleHzFit)^2));
+%     annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Fit coefficients with 95% confidence bounds: ', strtrim(str1+"   (Hz)"), strtrim(strrep(str2,"x0","\omega_0")+"   (Hz)"), strtrim(str3+"   (ms)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",18);
+%     hold on;
+%     plot(ax,ft,"r");
+%     legend("Real part of the spectrum", "Fit with Lorentzian", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
+% else
+%     legend("Real part of the spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
+% end
+% xlim([min(X_Hz_rel) max(X_Hz_rel)]);
+% xlabel("Chemical Shift (Hz)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18); 
+% ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+% 
+% saveas(fig, path+chemicalSpecies+"_Hz.fig");
+% saveas(fig, path+chemicalSpecies+"_Hz.svg");
+% saveas(fig, path+chemicalSpecies+"_Hz.png");
+% 
+% close all;
 
 %Plot in ppm
 fig = figure('WindowState', 'maximized');
 ax = gca;
 plot(ax, X_ppm_rel, sReal);
-title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 16);
-fitfunction="(1/pi)*((FWHM/2)/((x/"+num2str(scalePpmFit)+"-x0/"+num2str(scalePpmFit)+")^2+(FWHM/2)^2))";
-coeffs=["FWHM" "x0"];
-options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf],'Upper',[inf inf],'StartPoint',[1 rf_center_ppm]);
+title("Spectrum of "+chemicalSpecies, "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+fitfunction="COff+(1/pi)*((FWHM/2)/((x/"+num2str(scalePpmFit)+"-x0/"+num2str(scalePpmFit)+")^2+(FWHM/2)^2))";
+%fitfunction="(1/pi)*((FWHM/2)/((x/"+num2str(scalePpmFit)+"-x0/"+num2str(scalePpmFit)+")^2+(FWHM/2)^2))";
+coeffs=["FWHM" "x0" "COff"];
+options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf ppmyoffset],'Upper',[inf inf ppmyoffset],'StartPoint',[1 rf_center_ppm ppmyoffset]);
+%coeffs=["FWHM" "x0"];
+%options=fitoptions('Method','NonlinearLeastSquares','Lower',[-inf -inf],'Upper',[inf inf],'StartPoint',[1 159]);%rf_center_ppm
 fttype = fittype(fitfunction,coefficients=coeffs);
 X_ppm_t = transpose(X_ppm_rel);
 if attemptLorentzianFit
-    ft=fit(X_ppm_t,sReal,fttype,options);
+    outliersLorentzian = excludedata(X_ppm_t,sReal,'domain',notOutlierDomain);
+    ft=fit(X_ppm_t(~outliersLorentzian),sReal(~outliersLorentzian),fttype,options);
     coeffs = coeffnames(ft);
     coeffvals = coeffvalues(ft);
     ci = confint(ft,0.95);
@@ -210,20 +225,39 @@ if attemptLorentzianFit
     str1 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{1},coeffvals(1)*scalePpmFit,ci(:,1)*scalePpmFit); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
     str2 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',coeffs{2},coeffvals(2),ci(:,2)); %Skalierung passt so -> Lorentzian kann entsprechend umgestellt werden
     str3 = sprintf('\n %s = %0.9f   (%0.9f   %0.9f)',"Therefore T_2^*",1000/(coeffvals(1)*scalePpmFit*pi),1000*ci(:,1)*scalePpmFit/(pi*(coeffvals(1)*scalePpmFit)^2));
-    annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Fit coefficients with 95% confidence bounds: ', strtrim(str1+"   (Hz)"), strtrim(strrep(str2,"x0","\omega_0")+"   (ppm)"), strtrim(str3+"   (ms)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",8);
+    annotation('textbox',[0.53+annotationXOffset 0.69 0.2 0.2],'String',['Fit coefficients with 95% confidence bounds: ', strtrim(str1+"   (Hz)"), strtrim(strrep(str2,"x0","\omega_0")+"   (ppm)"), strtrim(str3+"   (ms)")],'EdgeColor','none',"FitBoxToText","on", "Color","r","FontSize",14);
     hold on;
     plot(ax,ft,"r");
-    legend("Real part of the spectrum", "Fit with Lorentzian", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
+    if useFindPeaks
+        [~,~,FWHMofPeak,~]=findpeaks(sReal(~outliersLorentzian),X_ppm_t(~outliersLorentzian),'Annotate','extents','WidthReference','halfheight','MinPeakHeight',minPeakHeight);
+        findpeaks(sReal(~outliersLorentzian),X_ppm_t(~outliersLorentzian),'Annotate','extents','WidthReference','halfheight','MinPeakHeight',minPeakHeight);
+        ax = gca;
+        sig_h = findobj(ax, 'tag', 'Signal');
+        set(sig_h, 'Color', 'm');
+        peak_h = findobj(ax, 'tag', 'Peak');
+        set(peak_h, 'Color', 'm');
+        set(peak_h, 'MarkerFaceColor', 'm');
+        fwhm_h = findobj(ax, 'tag', 'HalfHeightWidth');
+        set(fwhm_h, 'Color', 'm');
+        height_h = findobj(ax, 'tag', 'Height');
+        set(height_h, 'Color', 'm');
+        annotation('textbox',[0.53+annotationXOffset 0.4 0.2 0.2],'String',['FWHM from findpeaks: ',num2str((10^(-6))*FWHMofPeak*f_ref),' Hz'],'EdgeColor','none',"FitBoxToText","on", "Color","m","FontSize",14);
+    end
+    legend("Real Part of the Spectrum", "Fit with Lorentzian", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
 else
-    legend("Real part of the spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 10, "Location", "Northwest");
+    legend("Real Part of the Spectrum", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18, "Location", "Northwest");
 end
 xlim([min(X_ppm_rel) max(X_ppm_rel)]);
-xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14); 
-ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 14);
+xlabel("Chemical Shift (ppm)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18); 
+ylabel("Amplitude (a. u.)", "interpreter", "latex", 'fontweight', 'bold', 'fontsize', 18);
+ax = gca;
+ax.FontSize = 18;
+grid off;
+drawnow;
 
-saveas(fig, path+chemicalSpecies+"_ppm.fig");
-saveas(fig, path+chemicalSpecies+"_ppm.svg");
-saveas(fig, path+chemicalSpecies+"_ppm.png");
+saveas(fig, path+chemicalSpecies+additionalFileString+"_ppm.fig");
+saveas(fig, path+chemicalSpecies+additionalFileString+"_ppm.svg");
+saveas(fig, path+chemicalSpecies+additionalFileString+"_ppm.png");
 
 close all;
 
